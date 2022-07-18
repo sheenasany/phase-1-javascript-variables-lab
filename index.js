dd043b03baa99f1039ace90fc53f8b04740f1b88
Sheena Sang <sheenasany@gmail.com>
2022-07-17 22:33:17 -0400

    commit tree 204347e07a0e851949a606a232db8d00f6c75e85

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+
+// companyName is set and defined as 'Scuber'
+const companyName = 'Scuber'
+  
+// mostProfitableNeighborhood is declared as equal to 'Chelsea' using let
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+// companyCeo is declared as equal to 'Susan Smith' using let
+
+let companyCeo = 'Susan Smith'
